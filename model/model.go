f6dc026e52930fb83daff4f34a892ef20fdb5ad2
Yeghishe Piruzyan <ypiruzyan@gmail.com>
2016-05-03 18:20:25 -0700

    small change

--- model/model.go
+++ model/model.go
@@ -3,4 +3,6 @@ package model
 type Event struct {
 	EventType string `json:"type" binding:"required"`
 	Payload   string `json:"payload" binding:"required"`
+	FromNumber   string `json:"fromNumber" binding:"required"`
+	ToNumber   string `json:"toNumber" binding:"required"`
 }
